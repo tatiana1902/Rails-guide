be7595fe15bc4d639af14767d818f1b32457d78f
tatiana <tatianamoraacosta19@gmail.com>
2022-05-10 08:31:32 -0500

    second part of the rails guide, a CRUD, refactoring and security was carried out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles do
+    resources :comments
+  end
+  
 end
